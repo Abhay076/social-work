5dc897e16db2820cf95d93049876e2073aca1370
Abhay 076 <yadavabhaycs@gmail.com>
2023-03-01 09:00:15 +0530

    controllers and routes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
-app.listen(port, function(err){
-    if (err) {
-        console.log(`${err}`)
-        return
-    }
-    console.log(`Server has been Start at ${port}`);
-})
+
+//use express router
+app.use("/", require("./routes"));
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`${err}`);
+    return;
+  }
+  console.log(`Server has been Start at ${port}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const homeController = require("../controllers/home_controller");
+console.log("Router is loaded");
+router.get("/", homeController.home);
+//any of the futher routes used like this
+router.use('/users', require('./users'));
+
+module.exports = router;
